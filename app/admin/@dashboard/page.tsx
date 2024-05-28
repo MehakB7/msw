d869c39d3402027eeeb94d934b4a59e7855a49dc
mehakb7 <mehak@founderandlightning.com>
2024-05-28 13:36:55 +0530

    added error page for dashboard

--- app/admin/@dashboard/page.tsx
+++ app/admin/@dashboard/page.tsx
@@ -2,7 +2,8 @@ import { wait } from '@/app/_lib'
 import React from 'react'
 
 const page = async() => {
-  await wait(10000);
+  await wait(5000);
+   throw Error('Don;t render dashboard page')
   return (
     <div className="p-6 max-w-sm mx-auto bg-purple-600 rounded-xl shadow-md flex items-center space-x-4 w-4/12">
     <div className="pt-8 text-base font-semibold">
