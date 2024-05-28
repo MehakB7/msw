a97cfc47ae71712279b94fcf503d6aa438d52ff4
mehakb7 <mehak@founderandlightning.com>
2024-05-28 13:07:16 +0530

    added parallel routes

---
+++ app/admin/@dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { wait } from '@/app/_lib'
+import React from 'react'
+
+const page = async() => {
+  await wait(10000);
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-purple-600 rounded-xl shadow-md flex items-center space-x-4 w-4/12">
+    <div className="pt-8 text-base font-semibold">
+         <p className="text-white-900">Hey m Dashboard</p>
+     </div>
+   </div>
+  )
+}
+
+
+
+export default page
---
+++ app/admin/@team/loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const loading = () => {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-pink-400 rounded-xl shadow-md flex items-center space-x-4 w-4/12">
+    <div className="pt-8 text-base font-semibold">
+         <p className="text-white-900">loading team</p>
+     </div>
+   </div>
+  )
+}
+
+loading.propTypes = {}
+
+export default loading
