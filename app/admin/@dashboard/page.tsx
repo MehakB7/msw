fd890d0eb6383cd28f5f74028c5e050751b5062b
mehakb7 <mehak@founderandlightning.com>
2024-05-29 13:08:06 +0530

    remove error on dashboard

--- app/admin/@dashboard/page.tsx
+++ app/admin/@dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = async() => {
   await wait(5000);
-   throw Error('Don;t render dashboard page')
+  //  throw Error('Don;t render dashboard page')
   return (
     <div className="p-6 max-w-sm mx-auto bg-purple-600 rounded-xl shadow-md flex items-center space-x-4 w-4/12">
     <div className="pt-8 text-base font-semibold">
