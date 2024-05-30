3eabbcf939b2ec475828a3fb162df97e358b9f82
mehakb7 <mehak@founderandlightning.com>
2024-05-30 18:54:11 +0530

    added resume sections

---
+++ app/resume/constant.ts
@@ -0,0 +1,72 @@
+import { education, workIcon } from "../(dashboard)/icons";
+
+export const resume = [
+     {
+        title: "Work",
+        icon:workIcon,
+        data: [
+            {
+                title: "Software Engineer",
+                company: "Founder and lighting",
+                location: "Mohali, India",
+                duration: "Jan 2024 - Present",
+                image: "/images/plus-orange.svg",
+                skills:["Next-js", "Tailwind-css", "Typescript", "SEO"]
+            },
+            {
+                title: "Software Engineer",
+                company: "Winux Software solutions Pvt Ltd",
+                location: "Mohali, India",
+                duration: "Fab 2022 - Dec 2023",
+                image :"/images/winux.png",
+                skills:["React-js", "Redux", "Typescript", "Redux-toolkit", "Elastic-search"]
+            },
+            {
+                title: "Assosicate Software Engineer",
+                company: "Chicmic",
+                location: "Mohali, India",
+                duration: "Sept 2019 - Oct 2021",
+                image:"/images/chicmic.png",
+                skills: ["Javascript", "Typescript", "React-js","Cocos2d-js"]
+            },
+            {
+                title: " Intern",
+                company: "Chicmic",
+                location: "Mohali, India",
+                duration: "Jan 2019 - Sept 2019",
+                image:"/images/chicmic.png",
+                skills: ["Javascript", "Html", "CSS", "Cocos2d-js"]
+            },
+        ]
+    },
+    {
+        title: "Education",
+        icon:education,
+        data: [
+            {
+                title: "Ethereum Developer Bootcamp",
+                company: "Alchemy University",
+                location: "Remote",
+                duration: "Feb 2023 - Aug 2023",
+                image:"/images/al.png",
+                skills: ["Solidity", "Ethereum", "Web3.js"]
+            },
+            {
+                title: "B.E",
+                company: "Punjab University",
+                location: "Chandigarh, India",
+                duration: "2015 - 2019",
+                image:"/images/pu-logo.png",
+                skills: ["C","C++", "Algorithms", "Computer Programming"]
+            },
+            {
+                title: "12th",
+                company: "MCM D.A.V Public School",
+                location: "Kangra, India",
+                duration: "2013",
+                image:"/images/hp.png"
+            },
+        ]
+    }
+
+]
---
+++ app/resume/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { resume } from "./constant";
+import TimelineCard from "@/commonComponents/TimelineCard/timelineCard";
+function page() {
+  return (
+    <div className="container m-auto">
+      {resume.map((item, index) => (
+        <div key={index} className="max-lg:m-1">
+          <div className="flex items-center gap-4">
+          <h1 className="inline-block mb-2 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white">{item.title}</h1>
+          {item.icon}
+          </div>
+          <div className="m-4 max-lg:m-8">
+          <ol className="relative border-s border-gray-200 dark:border-gray-700"> 
+          {item.data.map((data, index)=>(
+           <TimelineCard {...data} key={index} />
+          )
+        )}
+         </ol>
+         </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default page;
