67e890e4eb8ea5c94a1e818fcc7ab52d0cad01ad
mehakb7 <mehak@founderandlightning.com>
2024-05-30 19:08:33 +0530

    remove the wait for about

--- app/(dashboard)/about/page.tsx
+++ app/(dashboard)/about/page.tsx
@@ -1,9 +1,8 @@
-import { wait } from '@/app/_lib'
+
 import Link from 'next/link'
 import React from 'react'
 
 const page = async() => {
-    await  wait(300000)
   return (
     <>
     <Link href="/">Home</Link>
