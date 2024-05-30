83c5fd8c189ab3d9e7dfb872953a2b612cc7b147
mehakb7 <mehak@founderandlightning.com>
2024-05-30 14:30:25 +0530

    added particle container and lang

---
+++ utils/lang.ts
@@ -0,0 +1,12 @@
+export  const lang = {
+    name: "Mehak",
+    profession: "Frontend Developer",
+    aboutMe: "About Me",
+    location: "Mohali, India",
+    companyName: "Founder and Lighting",
+    experience: "5 years",
+    greetings: "Hi there 👋 I'm Mehak.",
+    about: " I'm a frontend Developer. Currently working in Founder and Lighting. I have been doing development for last 5 years. Wrote my first piece of code 8 years ago. At first, it was scary but with time I learned and started to enjoy coding"
+}
+
+
