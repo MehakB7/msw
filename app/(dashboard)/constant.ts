f05c52d4402725cf41e6592d5ed48d2393ed1901
mehakb7 <mehak@founderandlightning.com>
2024-05-30 14:29:46 +0530

    added icons and constants

---
+++ app/(dashboard)/constant.ts
@@ -0,0 +1,29 @@
+import { github, leetcode, linkedin, mail } from "./icons";
+
+export const Links = [
+    {
+        name:"Mail",
+        icon: mail,
+        href: "mehakbisht5@gmail.com",
+        value: "mehakbisht5@gmail.com"
+    },
+    {
+        name:"linkedIn",
+        icon: linkedin,
+        href: "https://www.linkedin.com/in/mehakb7",
+        value: "mehakb7"
+    },
+    {
+        name:"github",
+        icon: github,
+        href: "https://github.com/MehakB7",
+        value: "mehakb7"
+    },
+
+    {
+        name:"leetcode",
+        icon: leetcode,
+        href: "https://leetcode.com/u/mehakb7/",
+        value: "mehakb7"
+    }
+]
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import { wait } from '@/app/_lib'
-import Link from 'next/link';
+import React from "react";
+import { wait } from "@/app/_lib";
+import { ParticleContainer } from "@/commonComponents/ParticleContainer/ParticleContainer";
+import Image from "next/image";
+import { Links } from "./constant";
+import Link from "next/link";
+import { location } from "./icons";
+import { lang } from "@/utils/lang";
 
- const Page = async() => {
-  await wait(100000);
+const {name, profession, aboutMe, about, location: loc, greetings } = lang;
+
+const Page = async () => {
+  await wait(1000);
   return (
     <>
-    <Link href="/about">About</Link>
-    <div>page1</div>
+      <ParticleContainer />
+      <div className="container mx-auto p-5 grid grid-cols-3 p-10">
+        <div className="flex flex-col justify-self-start items-center">
+          <Image
+            src="/images/profile.webp"
+            alt="hero"
+            width={256} 
+            height={300}
+            className="rounded-full"
+          />
+          <h1 className="text-4xl font-bold text-white">{name}</h1>
+          <p className="text-white">{profession}</p>
+          <div className="flex items-center gap-1"> {location}<span className="text-white">{loc}</span></div> 
+          <div className="flex gap-2 justify-center mt-4">
+          {Links.map((link) => <Link href = {link.href} key={link.name} target="_blank">
+            {link.icon}
+            </Link>)
+          }
+        
+          </div>
+          
+        </div>
+        <div className="w-full col-span-2">
+           <div className="shadow-2xl shadow-cyan-500/50 p-8 flex flex-col gap-y-4">
+            <h1 className="text-4xl font-bold text-white border-b-2">{aboutMe}</h1>
+            <p>{greetings}</p>
+            <p>{about}</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Page;
-
