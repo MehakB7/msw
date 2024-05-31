76a7a219aedbbad638788205c75e161827e9d77d
mehakb7 <mehak@founderandlightning.com>
2024-05-31 12:51:30 +0530

    added layout files for meta tag

--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -1,8 +1,8 @@
 import {ReactNode} from 'react'
 
  export const metadata ={
-    title : "Mehak",
-    description : "Mehak's portfolio"
+    title : "Resume",
+    description : "myresume"
  }
 
 
