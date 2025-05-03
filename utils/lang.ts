cd8da7aa200ed1ac8a683f467a7bf9a0a0a5ae22
mehakb7 <mehak@founderandlightning.com>
2025-05-03 22:49:25 +0530

    updated about me

--- utils/lang.ts
+++ utils/lang.ts
@@ -6,8 +6,9 @@ export  const lang = {
     companyName: "Founder and Lighting",
     experience: "5 years",
     greetings: "Hi there 👋 I'm Mehak.",
-    about: " I'm a frontend Developer. Currently working in Founder and Lighting. I have been doing development for last 5 years. Wrote my first piece of code 8 years ago. At first, it was scary but with time I learned and started to enjoy coding",
+    about: "I'm  a passionate Frontend Developer with over 5 years of hands-on experience in crafting responsive, user-friendly web interfaces.Currently, I’m working at Founder and Lighting, where I focus on building seamless digital experiences.My coding journey began in 2015, starting with a mix of curiosity and hesitation. That first line of code felt intimidating, but it sparked a fascination that’s only grown stronger. Over time, I transformed that curiosity into a deep love for frontend development — blending logic with creativity to bring ideas to life.I’m always excited to learn new tools, solve complex problems, and build products that make a difference.",
     cv: 'My CV',
 }
+
 
 
